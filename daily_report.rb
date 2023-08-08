fd9e4b8ea83a910fe5d2093812cb3c071e904fc2
Lindsay Holmwood <lindsay@holmwood.id.au>
2023-08-08 19:49:54 +1000

    Change report to show new courses in the last week

--- daily_report.rb
+++ daily_report.rb
@@ -5,7 +5,7 @@ course_count = ScraperWiki.select("count(id) AS count FROM courses").first["coun
 workshop_count = ScraperWiki.select("count(id) AS count FROM workshops").first["count"]
 imt_workshops = ScraperWiki.select("* FROM workshops WHERE course_id > 105769 AND course_id NOT IN (105788, 105789)")
 wicc_workshops = ScraperWiki.select("* FROM workshops WHERE course_id = 105644 ORDER BY start_date")
-new_workshops = ScraperWiki.select("* FROM workshops WHERE first_seen_at >= '#{Date.today.to_s}'")
+new_workshops = ScraperWiki.select("* FROM workshops WHERE first_seen_at >= '#{(Date.today - 7).to_s}'")
 
 def format(workshops, title:)
   keys = %w[name location date_freetext zone]
@@ -20,7 +20,7 @@ end
 puts "Total courses: #{course_count}"
 puts "Total workshops: #{workshop_count}"
 puts
-puts Terminal::Table.new(format(new_workshops, title: "New today"))
+puts Terminal::Table.new(format(new_workshops, title: "New in last 7 days"))
 puts
 puts Terminal::Table.new(format(imt_workshops, title: "IMT workshops"))
 puts
